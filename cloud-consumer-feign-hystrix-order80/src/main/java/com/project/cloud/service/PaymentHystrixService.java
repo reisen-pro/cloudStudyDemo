31a188b6ffdb55cd70866823c50f129791b801b9
reisen-pro <1054004534@qq.com>
2020-09-01 01:14:10 +0800

    hystrix配置熔断实现类，hystrix理论笔记

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/project/cloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/project/cloud/service/PaymentHystrixService.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @date 2020/8/30 23:02
  */
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT", fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService {
 
     @GetMapping(value = "/payment/hystrix/ok/{id}")
