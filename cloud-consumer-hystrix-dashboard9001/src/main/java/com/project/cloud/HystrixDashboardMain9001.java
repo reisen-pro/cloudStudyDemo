3317def2acfd3831be08b45dc13820fedd7d2b74
reisen-pro <1054004534@qq.com>
2020-09-02 23:39:43 +0800

    Hystrix Dashboard监控模块启用。

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/project/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.project.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Reisen
+ * @title: HystrixDashboardMain9001
+ * @projectName cloud
+ * @description: TODO
+ * @date 2020/9/2 23:35
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
