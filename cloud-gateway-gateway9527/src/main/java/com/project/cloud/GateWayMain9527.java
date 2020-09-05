caa845e57e91fb71512bbf7e9053f1236a76c52d
reisen-pro <1054004534@qq.com>
2020-09-05 23:34:32 +0800

    gateway相关配置，工程spring boot版本和spring cloud版本更新。

---
+++ cloud-gateway-gateway9527/src/main/java/com/project/cloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.project.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Reisen
+ * @title: GateWayMain9527
+ * @projectName cloud
+ * @description: TODO
+ * @date 2020/9/5 22:47
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
