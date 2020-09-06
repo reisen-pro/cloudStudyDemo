2d1e8e7fa5757aa9d0eeecdc371855f37c2d8233
reisen-pro <1054004534@qq.com>
2020-09-06 22:01:00 +0800

    Gateway自定义filter 更新mysql版本

---
+++ cloud-gateway-gateway9527/src/main/java/com/project/cloud/config/filter/MyLogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.project.cloud.config.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Reisen
+ * @title: MyLogGateWayFilter
+ * @projectName cloud
+ * @description: TODO
+ * @date 2020/9/6 21:41
+ */
+@Slf4j
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("************* come in MyLogGateWayFilter:{}", LocalDateTime.now());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            log.info("******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
