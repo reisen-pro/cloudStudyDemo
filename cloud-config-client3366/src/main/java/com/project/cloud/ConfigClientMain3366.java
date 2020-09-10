fb0a873b136a04e7799a3c5bb0ea2e004b2d1537
reisen-pro <1054004534@qq.com>
2020-09-11 01:38:07 +0800

    client3366模块加入 rabbitmq配置 rabbitmq作为消息总线通知cloud config center 通知配置中心 配置中心通知client端

---
+++ cloud-config-client3366/src/main/java/com/project/cloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.project.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Reisen
+ * @title: ConfigClientMain3366
+ * @projectName cloud
+ * @description: TODO
+ * @date 2020/9/10 20:56
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
