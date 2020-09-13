62c09f23adf37802286b8ae0f306fef6ab1adbad
reisen-pro <1054004534@qq.com>
2020-09-13 22:19:39 +0800

    cloud-stream 8802 实现消费订阅模块的实现与测试

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/project/cloud/StreamMqMain8802.java
@@ -0,0 +1,18 @@
+package com.project.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Reisen
+ * @title: StreamMqMain8802
+ * @projectName cloud
+ * @description: mq消费者模块启动类
+ * @date 2020/9/13 18:07
+ */
+@SpringBootApplication
+public class StreamMqMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMqMain8802.class, args);
+    }
+}
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/project/cloud/service/impl/MessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/project/cloud/service/impl/MessageProviderImpl.java
@@ -5,8 +5,8 @@ import com.project.cloud.service.MessageProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
 
 import javax.annotation.Resource;
 
